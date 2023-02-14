1e693c53c642bddbaa68baa6d05fb1dc20921608
MaxTalberg <74320161+MaxTalberg@users.noreply.github.com>
2023-02-14 16:25:13 +0000

    Update dictionary.py

--- code/dictionary.py
+++ code/dictionary.py
@@ -1,6 +1,6 @@
 # Creating a Dictionary for identitied exoplanet and host star
 
-exo_Dict = {
+planetary_system_results = {
     'A': {'period': 13.175, 'xlim_min': 0.820, 'xlim_max': 1.000, 'transit_loc': 0.900, 'earth_mass': 0.390, 'earth_radii': 2.9723, 'semimajor': 0.1131742275332411},
     
     'B': {'period': 21.776, 'xlim_min': 0.685, 'xlim_max': 0.732, 'transit_loc': 0.712, 'earth_mass': 4.1, 'earth_radii': 4.825, 'semimajor': 0.1582100229262183},
@@ -9,6 +9,6 @@ exo_Dict = {
   
     'D': {'period': 41.023, 'xlim_min': 0.160, 'xlim_max': 0.260, 'transit_loc': 0.220, 'earth_mass': 9.6, 'earth_radii': 3.897, 'semimajor': 0.2413250316045367}}
 
-star_Dict = {
+celestial_system_results = {
     'solar_radii': 1.66 }
     
